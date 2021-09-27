de8821050ff9f22cdfbcef2d740115209b15e27a
Simon Isinger <simon.isinger@web.de>
2021-09-27 18:20:46 +0200

    Anpassung der Logik der Schnittstellen

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Service\PostService;
-use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function get(PostService $postService)
     {
-        return json_encode($postService->getAll());
+        //$postService->getAll();
+        return $postService->getAll();
     }
 
     public function create(PostService $postService)
     {
-
+        return json_encode($postService->create(request()->post('tweetMessage')));
     }
 }
--- app/Service/PostService.php
+++ app/Service/PostService.php
@@ -7,17 +7,32 @@ use App\Models\User;
 
 class PostService
 {
-    public function getAll()
+    public function getAll(): array
     {
-        return Post::all();
+        $posts = Post::orderByDesc('created_at')->get(['message','created_at', 'user_id']);
+        $postsArray = [];
+        foreach ($posts as $index => $post){
+            // Hier wird ein recht performancelastiger Weg eingeschlagen, weil eloquent sich weigert die daten des nutzers automatisch mit auszulesen
+            $postsArray[] = [
+                'message' => $post->message,
+                'name' => User::where('id','=', $post->user_id)->get('name')->firstOrFail()->name,
+                'created_at' => $post->created_at
+            ];
+        }
+        return $postsArray;
     }
 
-    public function create(String $postContent){
+    public function create(String $postContent): array
+    {
         $user = User::all()->random();
         $post = new Post;
         $post->user_id = $user->id;
         $post->message = $postContent;
         $post->save();
-        return $post;
+        return [
+            'message' => $post->message,
+            'name' => User::where('id','=', $post->user_id)->get('name')->firstOrFail()->name,
+            'created_at' => $post->created_at
+        ];
     }
 }
